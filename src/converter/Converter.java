b77d28eed7ea743f717b2106135a291e36dbaaf6
Lucas Ferreira <ferreiralucas090@gmail.com>
2022-10-21 23:08:29 -0300

    feat: add precedence() function

--- src/converter/Converter.java
+++ src/converter/Converter.java
@@ -1,14 +1,42 @@
 package converter;
 
+import structures.Stack;
+
 public class Converter {
     
-    private static String operators[] = {"^", "*", "/", "+", "-", "(", ")"};
+    private static String operators[] = {"^", "*", "/", "+", "-"};
+    private static Stack<String> stack = new Stack<>(200);
+    private static Stack<String> postfix = new Stack<>(200);
 
-    private static Boolean isOperator(String operator){
+    public static Stack<String> converter(String expression){
+        String infix[] = expression.trim().split("\\s");
+
+
+        return postfix;
+    }
+
+    private static String isOperator(String element){
         for(String op : operators){
-            if(operator == op){ return true; }
+            if(element.equals(op)){ return element; }
         }
-        return false;
+        return null;
+    }
+
+
+
+    private static Integer precendece(String operator){
+        switch (operator) {
+            case "^":
+                return 3;
+            case "*":
+            case "/":
+                return 2;
+            case "+":
+            case "-":
+                return 1;
+            default:
+                return 0;
+        }
     }
 
 }
