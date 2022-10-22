4b3fdf3e2b536f88d37b0ac2003a4909061b84a3
Lucas Ferreira <ferreiralucas090@gmail.com>
2022-10-22 00:48:07 -0300

    feat: add parse() and converter() functions

--- src/App.java
+++ src/App.java
@@ -1,11 +1,15 @@
 import java.util.Scanner;
 
+import converter.Converter;
+
 public class App {
     public static void main(String[] args) throws Exception {
         Scanner input = new Scanner(System.in);
 
         System.out.println("Type the expression in infix format: ");
         String expression = input.nextLine();
+
+        System.out.println(Converter.converter(expression));
 
         input.close();
     }
--- src/converter/Converter.java
+++ src/converter/Converter.java
@@ -4,25 +4,47 @@ import structures.Stack;
 
 public class Converter {
     
-    private static String operators[] = {"^", "*", "/", "+", "-"};
-    private static Stack<String> stack = new Stack<>(200);
-    private static Stack<String> postfix = new Stack<>(200);
+    //private static String operators[] = {"^", "*", "/", "+", "-"};
 
     public static Stack<String> converter(String expression){
-        String infix[] = expression.trim().split("\\s");
+        String infix[] = expression.split("\\s");
 
+        return parse(infix);
+    }
 
+    private static Stack<String> parse(String infix[]){
+        Stack<String> stack = new Stack<>(200);
+        Stack<String> postfix = new Stack<>(200);
+
+        for(String element : infix){
+            switch (element) {
+                case "(":
+                    stack.push(element);
+                    break;
+                case ")":
+                    String next;
+                    while (!(next = stack.pop()).equals("("))
+                        postfix.push(next);
+                    break;
+                case "+":
+                case "-":
+                case "*":
+                case "/":
+                case "^":
+                    while(!stack.isEmpty() && precendece(stack.peek()) >= precendece(element))
+                        postfix.push(stack.pop());
+                    stack.push(element);
+                    break;
+                default:
+                    postfix.push(element);
+            }
+        }
+
+        while(!stack.isEmpty())
+            postfix.push(stack.pop());
+        
         return postfix;
     }
-
-    private static String isOperator(String element){
-        for(String op : operators){
-            if(element.equals(op)){ return element; }
-        }
-        return null;
-    }
-
-
 
     private static Integer precendece(String operator){
         switch (operator) {
